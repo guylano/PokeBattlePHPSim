737d0830085af4d3c9d2f080e2d9e37e2f56a94f
guylano <70889381+guylano@users.noreply.github.com>
2022-05-23 12:01:39 +0200

    weaknesses and resistances added aswell

--- PokeBattle/resources/effectiveness.php
+++ PokeBattle/resources/effectiveness.php
@@ -12,6 +12,7 @@
 
 	    public function getStrength($type){
 	    	$strength = array();
+	    	print($type);
 	    	switch ($type) {
 			  case 'Normal':
 			    
@@ -78,58 +79,58 @@
 	    	$weakness = array();
 	    	switch ($type) {
 			  case 'Normal':
-			    
+			    $weakness = array('Rock', 'Steel');
 			    break;
 			  case 'Fighting':
-			    
+			    $weakness = array('Poison', 'Flying', 'Psychic', 'Bug', 'Fairy');
 			    break;
 			  case 'Flying':
-			    
+			    $weakness = array('Electric', 'Rock', 'Steel');
 			    break;
 			  case 'Poison':
-			    
+			    $weakness = array('Poison', 'Ground', 'Rock', 'Ghost');
 			    break;
 			  case 'Ground':
-			    
+			    $weakness = array('Grass', 'Bug');
 			    break;
 			  case 'Rock':
-			    
+			    $weakness = array('Fighting', 'Ground', 'Steel');
 			    break;
 			  case 'Bug':
-			    
+			    $weakness = array('Fire', 'Fighting', 'Poison', 'Flying', 'Ghost', 'Steel', 'Flying');
 			    break;
 			  case 'Ghost':
-			    
+			    $weakness = array('Dark');
 			    break;
 			  case 'Steel':
-			    
+			    $weakness = array('Fire', 'Water', 'Electric', 'Steel');
 			    break;
 			  case 'Fire':
-			    
+			    $weakness = array('Fire', 'Water', 'Rock', 'Dragon');
 			    break;
 			  case 'Water':
-			    
+			    $weakness = array('Water', 'Grass', 'Dragon');
 			    break;
 			  case 'Grass':
-			    
+			    $weakness = array('Fire', 'Grass', 'Poison', 'Flying', 'Bug', 'Dragon', 'Steel');
 			    break;
 			  case 'Electric':
-			    
+			    $weakness = array('Grass', 'Electric', 'Dragon');
 			    break;
 			  case 'Psychic':
-			    
+			    $weakness = array('Psychic', 'Steel');
 			    break;
 			  case 'Ice':
-			    
+			    $weakness = array('Fire','Water', 'Ice', 'Steel');
 			    break;
 			  case 'Dragon':
-			    
+			    $weakness = array('Steel');
 			    break;
 			  case 'Dark':
-			    
+			    $weakness = array('Fighting', 'Dark', 'Fairy');
 			    break;
 			  case 'Fairy':
-			    
+			    $weakness = array('Fire', 'Poison', 'Steel');
 			    break;
 			  
 			  default:
@@ -142,58 +143,58 @@
 	    	$resistance = array();
 	    	switch ($type) {
 			  case 'Normal':
-			    
+			    $resistance = array('Ghost');
 			    break;
 			  case 'Fighting':
-			    
+			    $resistance = array('Ghost');
 			    break;
 			  case 'Flying':
-			    
+			    $resistance = array();
 			    break;
 			  case 'Poison':
-			    
+			    $resistance = array('Steel');
 			    break;
 			  case 'Ground':
-			    
+			    $resistance = array('Flying');
 			    break;
 			  case 'Rock':
-			    
+			    $resistance = array();
 			    break;
 			  case 'Bug':
-			    
+			    $resistance = array();
 			    break;
 			  case 'Ghost':
-			    
+			    $resistance = array('Normal');
 			    break;
 			  case 'Steel':
-			    
+			    $resistance = array();
 			    break;
 			  case 'Fire':
-			    
+			    $resistance = array();
 			    break;
 			  case 'Water':
-			    
+			    $resistance = array();
 			    break;
 			  case 'Grass':
-			    
+			    $resistance = array();
 			    break;
 			  case 'Electric':
-			    
+			    $resistance = array('Ground');
 			    break;
 			  case 'Psychic':
-			    
+			    $resistance = array('Dark');
 			    break;
 			  case 'Ice':
-			    
+			    $resistance = array();
 			    break;
 			  case 'Dragon':
-			    
+			    $resistance = array('Fairy');
 			    break;
 			  case 'Dark':
-			    
+			    $resistance = array();
 			    break;
 			  case 'Fairy':
-			    
+			    $resistance = array();
 			    break;
 			  
 			  default:
